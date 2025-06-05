b2ddcb1838c789a5787c77b06b1ce4239e6c509f
GitCGuillaume <guillaume2.chopin@orange.fr>
2025-06-05 12:04:28 +0200

    add 3 files, now argocd install update password, + add the new yaml files, connection with 443

--- p3/run.sh
+++ p3/run.sh
@@ -9,11 +9,23 @@ fi
 
 if [ "$1" ]; then
     case "$1" in
-        "--create") k3d cluster create --config ./confs/k3d.yaml
-        kubectl create namespace argocd
-        kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.0.3/manifests/install.yaml
-        kubectl apply -f confs/ingress.yaml
-        kubectl apply -f confs/argocd-cmd-param-cm.yaml
+        "--create")
+        if [ -f .env ]; then
+            k3d cluster create --config ./confs/k3d.yaml
+            source .env
+            kubectl apply -f confs/argocd_ns.yaml
+            kubectl apply -f confs/dev_ns.yaml
+            kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.0.3/manifests/install.yaml
+            kubectl apply -f confs/ingress.yaml
+            kubectl apply -f confs/argocd-cmd-param-cm.yaml
+            echo "Waiting for argocd pods: kubectl -n argocd wait --for=condition=ready pod --all --timeout=600s"
+            kubectl -n argocd wait --for=condition=ready pod --all --timeout=600s
+            TMP_PSW=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+            argocd login kubernetes.docker.internal:8080 --grpc-web --username admin --password $TMP_PSW --insecure
+            argocd account update-password --grpc-web --new-password $PSW_ADM --current-password $TMP_PSW
+        else
+            echo "Please add a .env file with PSW_ADM=(8..32 characters).";
+        fi
         ;;
         "--delete") k3d cluster delete $name
         ;;
