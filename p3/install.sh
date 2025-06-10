c2776a69acaa752799d61a31cc5f809cee93a6e7
guillaume <guillaume@desktop-13efv45.home>
2025-06-10 14:03:01 +0200

    update install.sh file

--- p3/install.sh
+++ p3/install.sh
@@ -1,12 +1,23 @@
-var="$(ls /usr/local/bin/)"
 brew="$(which brew)"
+kube="$(which kubectl)"
+k3d="$(which k3d)"
+argo="$(which argocd)"
 
 if [ "$brew" ]; then
-    if [ "$var" ]; then
-        echo "$var";
-    else
+    if [ ! "$k3d" ]; then
         wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    else
+        echo "K3d is already installed.";
+    fi
+    if [ ! "$argo" ]; then
         brew install argocd
+    else
+        echo "ArgoCD is already installed.";
+    fi
+    if [ ! "$kube" ]; then
+        brew install kubectl
+    else
+        echo "Kubectl is already installed.";
     fi
 else
     echo "Please install brew: https://brew.sh/";
